647eb870a32210300384622300d8de6a7474caac
Darby Stephens <darbystephens@gmail.com>
2018-02-20 14:46:23 -0500

    initial commit

---
+++ components/form.component.js
@@ -0,0 +1,28 @@
+(function(){
+  var formComponent = {
+    template: `
+    <div>
+      <form>
+        <input type="radio" name="flannelName" />Flannel Day
+        <input type="radio" name="pajamaName" />Pajama Day
+        <input type="radio" name="hat" /> Hat Day
+      </form>
+    </div>
+    `,
+
+    controller: function(){
+      var $ctrl = this;
+      $ctrl.whatToWear(temperature, eventType){
+        
+      }
+
+    }
+
+
+
+  }
+
+
+
+
+})();
---
+++ services/apiService.service.js
@@ -0,0 +1,14 @@
+(function(){
+
+  $http.get({
+url: "https://simple-weather.p.mashape.com/weatherdata?lat=42.331427&lng=-83.045754",
+   headers: {
+        "X-Mashape-Key": "pkzJVhwpv3mshQP76JJemeAhZ4XNp1jqUHwjsnz8KjkE2bM6gW",
+        "Accept": "application/json"
+   }
+}).then(function(response) { console.log(response.data); });
+
+
+
+
+})();
